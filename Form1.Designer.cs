2f989136f5b523f481a37db06294ba962d36250b
joaomasters <30707586+joaomasters@users.noreply.github.com>
2020-10-07 14:28:54 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Gerenciando_SQL_Server_Compact_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCriarBD = new System.Windows.Forms.Button();
+            this.btnCriarTabelas = new System.Windows.Forms.Button();
+            this.btnCarregarTabela = new System.Windows.Forms.Button();
+            this.btnCarregarGrid = new System.Windows.Forms.Button();
+            this.btnLerRegistros = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCriarBD
+            // 
+            this.btnCriarBD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriarBD.Location = new System.Drawing.Point(277, 29);
+            this.btnCriarBD.Name = "btnCriarBD";
+            this.btnCriarBD.Size = new System.Drawing.Size(165, 34);
+            this.btnCriarBD.TabIndex = 0;
+            this.btnCriarBD.Text = "Criar Banco de Dados";
+            this.btnCriarBD.UseVisualStyleBackColor = true;
+            this.btnCriarBD.Click += new System.EventHandler(this.btnCriarBD_Click);
+            // 
+            // btnCriarTabelas
+            // 
+            this.btnCriarTabelas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriarTabelas.Location = new System.Drawing.Point(277, 69);
+            this.btnCriarTabelas.Name = "btnCriarTabelas";
+            this.btnCriarTabelas.Size = new System.Drawing.Size(165, 35);
+            this.btnCriarTabelas.TabIndex = 0;
+            this.btnCriarTabelas.Text = "Criar Tabelas";
+            this.btnCriarTabelas.UseVisualStyleBackColor = true;
+            this.btnCriarTabelas.Click += new System.EventHandler(this.btnCriarTabelas_Click);
+            // 
+            // btnCarregarTabela
+            // 
+            this.btnCarregarTabela.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregarTabela.Location = new System.Drawing.Point(277, 110);
+            this.btnCarregarTabela.Name = "btnCarregarTabela";
+            this.btnCarregarTabela.Size = new System.Drawing.Size(165, 37);
+            this.btnCarregarTabela.TabIndex = 0;
+            this.btnCarregarTabela.Text = "Carregar Tabela";
+            this.btnCarregarTabela.UseVisualStyleBackColor = true;
+            this.btnCarregarTabela.Click += new System.EventHandler(this.btnCarregarTabela_Click);
+            // 
+            // btnCarregarGrid
+            // 
+            this.btnCarregarGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregarGrid.Location = new System.Drawing.Point(277, 153);
+            this.btnCarregarGrid.Name = "btnCarregarGrid";
+            this.btnCarregarGrid.Size = new System.Drawing.Size(165, 39);
+            this.btnCarregarGrid.TabIndex = 0;
+            this.btnCarregarGrid.Text = "Carregar Grid";
+            this.btnCarregarGrid.UseVisualStyleBackColor = true;
+            this.btnCarregarGrid.Click += new System.EventHandler(this.btnCarregarGrid_Click);
+            // 
+            // btnLerRegistros
+            // 
+            this.btnLerRegistros.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLerRegistros.Location = new System.Drawing.Point(277, 198);
+            this.btnLerRegistros.Name = "btnLerRegistros";
+            this.btnLerRegistros.Size = new System.Drawing.Size(165, 35);
+            this.btnLerRegistros.TabIndex = 0;
+            this.btnLerRegistros.Text = "Ler Registros";
+            this.btnLerRegistros.UseVisualStyleBackColor = true;
+            this.btnLerRegistros.Click += new System.EventHandler(this.btnLerRegistros_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resultado";
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(16, 250);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(426, 150);
+            this.dgvDados.TabIndex = 2;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.Blue;
+            this.lblResultado.Location = new System.Drawing.Point(16, 35);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(246, 198);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(454, 412);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLerRegistros);
+            this.Controls.Add(this.btnCarregarGrid);
+            this.Controls.Add(this.btnCarregarTabela);
+            this.Controls.Add(this.btnCriarTabelas);
+            this.Controls.Add(this.btnCriarBD);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Server Compact  - Criando e acessando Banco de dados, Tabelas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCriarBD;
+        private System.Windows.Forms.Button btnCriarTabelas;
+        private System.Windows.Forms.Button btnCarregarTabela;
+        private System.Windows.Forms.Button btnCarregarGrid;
+        private System.Windows.Forms.Button btnLerRegistros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace Gerenciando_SQL_Server_Compact_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCriarBD_Click(object sender, EventArgs e)
+        {
+            /* define os parâmetros para o inputbox */
+            string Prompt = "Informe o nome do Banco de Dados a ser criado.Ex: Teste.sdf";
+            string Titulo = "www.macoratti.net";
+            string Resultado = Interaction.InputBox(Prompt, Titulo, @"c:\dados\Agenda.sdf", 650, 350);
+            /* verifica se o resultado é uma string vazia o que indica que foi cancelado. */
+            if (Resultado != "")
+            {
+                if (!Resultado.Contains(".sdf"))
+                {
+                    MessageBox.Show("Informe a extensão .sdf no arquivo...");
+                    return;
+                }
+                try
+                {
+                    string connectionString;
+                    string nomeArquivoBD = Resultado;
+                    string senha = "";
+
+                    if (File.Exists(nomeArquivoBD))
+                    {
+                        if (MessageBox.Show("O arquivo já existe !. Deseja excluir e criar novamente ? ", "Excluir", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                        {
+                            File.Delete(nomeArquivoBD);
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+
+                    connectionString = string.Format("DataSource=\"{0}\"; Password='{1}'", nomeArquivoBD, senha);
+
+                    if (MessageBox.Show("Será criado arquivo " +  connectionString  +" Confirma ? ", "Criar", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                    {
+                        SqlCeEngine SqlEng = new SqlCeEngine(connectionString);
+                        SqlEng.CreateDatabase();
+                        lblResultado.Text = "Banco de dados " + nomeArquivoBD + " com sucesso !";
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("A operação foi cancelada...");
+            }
+         }
+
+        private void btnCriarTabelas_Click(object sender, EventArgs e)
+        {
+                /* define os parâmetros para o inputbox */
+                string Prompt = "Informe o nome da tabela a ser criada.Ex: Teste";
+                string Titulo = "www.macoratti.net";
+                string Resultado = Interaction.InputBox(Prompt, Titulo, "Contatos", 650, 350);
+                /* verifica se o resultado é uma string vazia o que indica que foi cancelado. */
+
+                if (Resultado != "")
+                {
+                    if (Resultado.Contains(".sdf"))
+                    {
+                        MessageBox.Show("Não informe a extensão .sdf no arquivo...");
+                        return;
+                    }
+
+                    SqlCeConnection cn = new SqlCeConnection(stringConexao());
+
+                    if (cn.State == ConnectionState.Closed)
+                    {
+                        cn.Open();
+                    }
+                    SqlCeCommand cmd;
+
+                    string sql = "create table " + Resultado + "("
+                               + "Nome nvarchar (60) not null, "
+                               + "Sobrenome nvarchar (80), "
+                               + "URL nvarchar (150) )";
+
+                    cmd = new SqlCeCommand(sql, cn);
+
+                    if (MessageBox.Show("Confirma a criação da tabela ? ", "Criar Tabela", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                    {
+                        try
+                        {
+                            cmd.ExecuteNonQuery();
+                            lblResultado.Text = "Tabela " + Resultado + " criada com sucesso ";
+                        }
+                        catch (SqlCeException sqlexception)
+                        {
+                            MessageBox.Show(sqlexception.Message, "Caramba.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Caramba.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            cn.Close();
+                        }
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("A operação foi cancelada...");
+                }
+        }
+
+         private string stringConexao()
+         {
+             string connectionString="";
+             try
+             {
+                 string nomeArquivo = @"c:\dados\Agenda.sdf";
+                 string senha = "";
+                 connectionString = string.Format("DataSource=\"{0}\"; Password='{1}'", nomeArquivo, senha);
+             }
+             catch (Exception ex)
+             {
+                 MessageBox.Show(ex.Message);
+             }
+             return connectionString;
+         }
+
+         private void btnCarregarTabela_Click(object sender, EventArgs e)
+         {
+                try
+                {
+                    CarregarLinha("Macoratti", "Jose Carlos", @"http:\\www.macoratti.net");
+                    CarregarLinha("Janice", "Rachel", @"janjan@uol.com.br");
+                    CarregarLinha("Jefferson", "Andre", @"jeff@bol.com.br");
+                    CarregarLinha("Miriam", "Estela", @"mimi@hotmail.com");
+                    CarregarLinha("Jessica", "Naara", @"jessicalang@ig.com.br");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Oxente.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+         }
+         private void CarregarLinha(string nome, string sobrenome, string url)
+         {
+                SqlCeConnection cn = new SqlCeConnection(stringConexao());
+                if (cn.State == ConnectionState.Closed)
+                {
+                    cn.Open();
+                }
+
+                SqlCeCommand cmd;
+                string sql = "insert into Contatos "
+                            + "(sobrenome, nome, url) "
+                            + "values (@sobrenome, @nome, @url)";
+                try
+                {
+                    cmd = new SqlCeCommand(sql, cn);
+                    cmd.Parameters.AddWithValue("@sobrenome", sobrenome);
+                    cmd.Parameters.AddWithValue("@nome", nome);
+                    cmd.Parameters.AddWithValue("@url", url);
+                    cmd.ExecuteNonQuery();
+                    lblResultado.Text = "Linha Incluída.";
+                }
+                catch (SqlCeException sqlexception)
+                {
+                    MessageBox.Show(sqlexception.Message, "Uaí Sô.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Uaí Sô.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                cn.Close();
+                }
+          }
+
+        private void btnCarregarGrid_Click(object sender, EventArgs e)
+        {
+                SqlCeConnection cn = new SqlCeConnection(stringConexao());
+                if (cn.State==ConnectionState.Closed)
+                {
+                    try
+                    {
+                        cn.Open();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                try
+                {
+                    // define o command par ausar a tabela e não a consulta
+                    SqlCeCommand cmd = new SqlCeCommand("Contatos", cn);
+                    cmd.CommandType = CommandType.TableDirect;
+
+                    // Pega a tabela
+                    SqlCeResultSet rs = cmd.ExecuteResultSet(ResultSetOptions.Scrollable);
+
+                    // carrega o resultado no grid 
+                    dgvDados.DataSource = rs;
+                }
+                catch (SqlCeException sqlexception)
+                {
+                    MessageBox.Show(sqlexception.Message, "Arre Égua.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Arre Égua.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+       }
+
+         private void btnLerRegistros_Click(object sender, EventArgs e)
+         {
+             SqlCeConnection cn = new SqlCeConnection(stringConexao());
+             if (cn.State == ConnectionState.Closed)
+             {
+                 cn.Open();
+             }
+             // Monta a consulta SQL 
+             string sql = "select sobrenome, nome from Contatos ";
+             try
+             {
+                 SqlCeCommand cmd = new SqlCeCommand(sql, cn);
+                 cmd.CommandType = CommandType.Text;
+                 SqlCeResultSet rs = cmd.ExecuteResultSet(ResultSetOptions.Scrollable);
+                 // se você precisa atualizar o result set então use:
+                 // SqlCeResultSet rs = cmd.ExecuteResultSet(ResultSetOptions.Scrollable | ResultSetOptions.Updatable);
+                 if (rs.HasRows)
+                 {
+                     int ordSobrenome = rs.GetOrdinal("sobrenome");
+                     int ordNome = rs.GetOrdinal("nome");
+                     // trata a saida
+                     StringBuilder saida = new StringBuilder();
+                     // le o primeiro registro e pega os dados
+                     rs.ReadFirst();
+                     saida.AppendLine(rs.GetString(ordNome) + " " + rs.GetString(ordSobrenome));
+                     while (rs.Read())
+                     {
+                         saida.AppendLine(rs.GetString(ordNome) + " " + rs.GetString(ordSobrenome));
+                     }
+                     // defina a saida
+                     lblResultado.Text = saida.ToString();
+                 }
+                 else
+                 {
+                     lblResultado.Text = "Nenhum registro encontrado.";
+                 }
+             }
+             catch (SqlCeException sqlexception)
+             {
+                 MessageBox.Show(sqlexception.Message, "Bah Tchê.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+             }
+             catch (Exception ex)
+             {
+                 MessageBox.Show(ex.Message, "Bah Tchê.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+             }
+             finally
+             {
+                 cn.Close();
+             }
+         }
+    }           
+}
